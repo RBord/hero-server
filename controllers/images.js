7b993075002451d57d8f697047ea9f9b26122915
RBord <rostyslavbord@gmail.com>
2021-11-25 17:14:08 +0200

    return jimp

--- controllers/images.js
+++ controllers/images.js
@@ -1,17 +1,17 @@
 const { Superhero } = require('../models/superhero')
 const path = require('path')
 const fs = require('fs/promises')
-// const Jimp = require('jimp')
+const Jimp = require('jimp')
 
 const addImages = async (req, res) => {
   const { description } = req.body
   const { id } = req.params
   const { path: tempDir, originalname } = req.file
   const avatarsDir = path.join(process.cwd(), 'public/images')
-  // Jimp.read(tempDir, (err, lenna) => {
-  //   if (err) throw err
-  //   lenna.resize(250, 250).quality(60)
-  // })
+  Jimp.read(tempDir, (err, lenna) => {
+    if (err) throw err
+    lenna.resize(300, 200).quality(60)
+  })
   try {
     const [extention] = originalname.split('.').reverse()
     const newAvatarName = `image-${id}.${extention}`
