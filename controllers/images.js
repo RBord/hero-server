441f288e019b11d74dc2fa73e99f6b90c989739b
RBord <rostyslavbord@gmail.com>
2021-11-29 20:03:50 +0200

    work with images

--- controllers/images.js
+++ controllers/images.js
@@ -9,24 +9,18 @@ const addImages = async (req, res) => {
   const { id } = req.params
   const file = req.file
   const result = await uploadFile(file)
-  // const { path: tempDir, originalname } = req.file
-  // const avatarsDir = path.join(process.cwd(), 'public/images')
-  // Jimp.read(tempDir, (err, lenna) => {
-  //   if (err) throw err
-  //   lenna.resize(300, 200).quality(60)
-  // })
-  // try {
-  //   const [extention] = originalname.split('.').reverse()
-  //   const newAvatarName = `image-${id}.${extention}`
-  //   const resultDir = path.join(avatarsDir, newAvatarName)
-  //   await fs.rename(tempDir, resultDir)
-
-  const image = path.join('/images', result.Key)
-  await Superhero.findByIdAndUpdate(id, { images: image })
-  res.json({
-    status: 200,
-    images: image,
-  })
+  const { path: tempDir } = req.file
+  const readStream = getFileStream(result.Key)
+  readStream.pipe(res)
+  try {
+    const resultDir = path.join('public/images', result.Key)
+    await fs.rename(tempDir, resultDir)
+    await Superhero.findByIdAndUpdate(id, { images: resultDir })
+    res.json({
+      status: 200,
+      images: resultDir,
+    })
+  } catch {}
 }
 
 module.exports = {
